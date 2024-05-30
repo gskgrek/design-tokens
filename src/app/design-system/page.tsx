d079b93fe42eac569ec73fe35bb2a3a92dd6786c
gskgrek <biuro@greku.pl>
2024-05-30 21:14:07 +0200

    design system theme

--- src/app/design-system/page.tsx
+++ src/app/design-system/page.tsx
@@ -2,13 +2,12 @@ import { ConfigProvider } from 'antd'
 
 import { Card } from "@/components/design-system"
 
+import theme from '@/configs/tokens/design-system/tokens.json'
+
 export default function FigmaVariables() {
   return (
     <ConfigProvider
-      theme={{
-        token: {},
-        components: {},
-      }}
+      theme={theme}
     >
       <Card />
     </ConfigProvider>
